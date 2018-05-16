6cfc085c08397081a7c5f52c53c8e14b5fa3f8d1
aydincan77 <aydincan77@gmail.com>
2018-05-16 22:43:55 +0300

    Initial Commit

---
+++ Kod ile Merhaba Dünya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kod ile Merhaba Dünya
+//
+//  Created by Aydın Can on 16.05.2018.
+//  Copyright © 2018 CAN Soft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
